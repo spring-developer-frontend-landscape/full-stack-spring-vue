81f96bd59873d99b52aa72ebeb8fefefd1d505f0
Dan Vega <danvega@gmail.com>
2024-05-23 20:28:50 -0400

    initial commit

---
+++ src/main/java/dev/danvega/FullStuckSpringVueApplication.java
@@ -0,0 +1,13 @@
+package dev.danvega;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullStuckSpringVueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullStuckSpringVueApplication.class, args);
+	}
+
+}
